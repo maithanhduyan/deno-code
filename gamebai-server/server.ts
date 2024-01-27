126e20cf45930b50fd4d1545eb6c715f8eb32118
Mai Thành Duy An <tiachop0102@gmail.com>
2024-01-27 15:54:39 +0900

    Create server game bai

--- gamebai-server/server.ts
+++ gamebai-server/server.ts
@@ -1,25 +1,103 @@
-Deno.serve((req) => {
-  if (req.headers.get("upgrade") != "websocket") {
-    return new Response(null, { status: 501 });
+/**
+ * gamebai-server deno project
+ */
+class Card {
+
+  name: string;
+
+  constructor(public rank: string, public suit: string) {
+    this.suit = suit;
+    this.rank = rank;
+    this.name = `${suit}${rank}`;
   }
 
-  const { socket, response } = Deno.upgradeWebSocket(req);
+  getName() {
+    return this.name;
+  }
+}
 
-  socket.addEventListener("open", () => {
-    console.log("a client connected!");
-  });
+interface Card {
+  rank: string;
+  suit: string;
+  getName(): string;
+}
 
-  socket.addEventListener("message", (event) => {
-    if (event.data === "ping") {
-      socket.send("pong");
-    } else {
-      socket.send("pong");
+class Deck {
+  cards: Card[];
+  constructor() {
+    this.cards = [];
+    this.createDeck();
+    this.shuffleDeck();
+  }
+
+  private createDeck(): void {
+    const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+    const ranks = ['3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace', '2'];
+
+    for (let suit of suits) {
+      for (let rank of ranks) {
+        this.cards.push(new Card(rank, suit));
+      }
     }
-  });
+  }
 
-  socket.addEventListener("close", () => {
-    console.log("a client disconnected.");
-  });
+  private shuffleDeck(): void {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+  }
 
-  return response;
+  public dealCards(numberOfCards: number): Card[] {
+    return this.cards.splice(0, numberOfCards);
+  }
+}
+
+const BOOK_ROUTE = new URLPattern({ pathname: "/books/:id" });
+const HOME_ROUTE = new URLPattern({ pathname: "/" });
+const WEB_SOCKET_ROUTE = new URLPattern({ pathname: "/websocket" });
+Deno.serve((req) => {
+  // 
+  const match = BOOK_ROUTE.exec(req.url);
+  const match_home_url = HOME_ROUTE.exec(req.url);
+  const match_websocket_url = WEB_SOCKET_ROUTE.exec(req.url);
+
+  if (match) {
+    const id = match.pathname.groups.id;
+    return new Response(`Book ${id}`);
+  }
+  if (match_home_url) {
+    return new Response("Welcome Deno Web App");
+  }
+
+  if (match_websocket_url) {
+    console.log("match_websocket_url");
+    if (req.headers.get("upgrade") != "websocket") {
+      return new Response(null, { status: 501 });
+    }
+
+    const { socket, response } = Deno.upgradeWebSocket(req);
+
+    socket.addEventListener("open", () => {
+      console.log("WebSocket client connected!");
+    });
+
+    socket.addEventListener("message", (event) => {
+      if (event.data === "ping") {
+        socket.send("pong");
+      }
+      if (event.data === "CHIA_BAI") {
+        socket.send("CHIA_BAI");
+      }
+    });
+
+    socket.addEventListener("close", (event) => {
+      console.log("WebSocket client disconnected.");
+    });
+
+    return response;
+  }
+
+  // Nếu không khớp với bất kỳ route nào
+  return new Response("Not Found", { status: 404 });
 });
