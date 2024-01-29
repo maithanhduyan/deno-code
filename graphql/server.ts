d3e934b8f562a2cad8517c7831c25e24274ee573
Mai Thành Duy An <tiachop0102@gmail.com>
2024-01-30 01:25:36 +0900

    Deno GraphQL sample.

---
+++ graphql/server.ts
@@ -0,0 +1,32 @@
+import { GraphQLHTTP } from 'https://deno.land/x/gql@1.2.4/mod.ts'
+import { makeExecutableSchema } from 'npm:@graphql-tools/schema@10.0.0'
+import { gql } from 'https://deno.land/x/graphql_tag@0.1.2/mod.ts'
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`
+
+const resolvers = {
+    Query: {
+        hello: () => `Hello World!`,
+    },
+}
+
+const schema = makeExecutableSchema({ resolvers, typeDefs })
+
+Deno.serve({
+    port: 3001,
+    onListen({ hostname, port }) {
+        console.log(`☁  Started on http://${hostname}:${port}`)
+    },
+}, async (req) => {
+    const { pathname } = new URL(req.url)
+    return pathname === '/graphql'
+        ? await GraphQLHTTP<Request>({
+            schema,
+            graphiql: true,
+        })(req)
+        : new Response('Not Found', { status: 404 })
+})
